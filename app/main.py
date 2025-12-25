116977e794d83890b8d9259ef3262ce9297be561
khoahotran <trannguyenanhkhoa0104@gmail.com>
2025-12-25 13:31:16 +0700

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from app.model import predict_review, predict_aspect_sentiment
 
 app = FastAPI(title="Vietnamese Sentiment API")
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ReviewRequest(BaseModel):
